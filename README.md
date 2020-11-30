a8b363575a76792a1c44cd11849ff07dde387640
Will Dane <enadlliw@gmail.com>
2020-11-30 16:09:37 -0500

    fixed markdown syntax and added badge and added my own readme for the project

---
+++ README.md
@@ -0,0 +1,44 @@
+# README Generator
+
+![badge](https://img.shields.io/badge/License-MIT-blue)
+
+## Description
+
+    Use this program to create a professional looking README.md for your project!
+
+## Table of Contents
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributors](#Contributors)
+- [Tests](#Tests)
+- [License](#License)
+- [Questions](#Questions)
+
+## Installation
+
+    npm install inquirer in your node.js terminal
+
+## Usage
+
+    In your node.js terminal, just type in "node index.js" and follow the prompts given to fill in the information that will be added to your README!
+
+## Contributors
+
+    For inquiries regarding contribution, please reach out to me on github or via my email listed below.
+
+## Tests
+
+    None
+
+## License
+
+    This project is licensed under MIT licensing rules.
+
+## Questions
+
+[wmdane](github.com/wmdane)
+
+willmdane@gmail.com
+
+![READMEScreen](https://user-images.githubusercontent.com/69867610/100664692-ff99ad00-3325-11eb-92c2-b01bb5ac93cb.png)
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,40 +1,41 @@
 function generateMarkdown(response) {
   return `
-    
-    # ${response.title}
-    
-    # Description
+  
+  # ${response.title}
+  ![badge](https://img.shields.io/badge/License-${response.license}-blue)
+
+  ## Description
     ${response.description}
-    
-    # Table of Contents
-    * [Installation](#Installation)
-    * [Usage](#Usage)
-    * [Contributors](#Contributors)
-    * [Tests](#Tests)
-    * [License](#License)
-    * [Questions](#Questions)
-    
-    # Installation
+  
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contributors](#Contributors)
+  * [Tests](#Tests)
+  * [License](#License)
+  * [Questions](#Questions)
+
+  ## Installation
     ${response.installation}\n
 
-    # Usage
+  ## Usage
     ${response.usage}\n
 
-    # Contributors
+  ## Contributors
     ${response.contributing}\n
-    
-    # Tests
+  
+  ## Tests
     ${response.tests}\n
 
-    # License
-    ${response.license}\n
+  ## License
+    This project is licensed under ${response.license} licensing rules.\n
 
-    # Questions
-    ${response.github}
-    ${response.email}
+  ## Questions
+  [${response.github}](github.com/${response.github})\n
+  ${response.email}
 
-    
-    `;
+  
+  `;
 }
 
 module.exports = generateMarkdown;
